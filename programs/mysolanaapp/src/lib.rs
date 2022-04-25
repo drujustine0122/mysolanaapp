0abf8760d2646bf5a1e8f1d337edde6ab790bbb2
snowdev1205 <snowdev1205@gmail.com>
2022-04-24 21:38:09 -0500

    backend complete

--- programs/mysolanaapp/src/lib.rs
+++ programs/mysolanaapp/src/lib.rs
@@ -1,15 +1,50 @@
 use anchor_lang::prelude::*;
+use solana_program::{
+    entrypoint::ProgramResult
+};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+declare_id!("47hwQKg6fkRPXMkGSttDxhwtqzy7a6Epa9JGQFwBbDV8");
 
 #[program]
 pub mod mysolanaapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(
+        ctx: Context<Create>,
+    ) ->  ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
         Ok(())
     }
+
+    pub fn increment(
+        ctx: Context<Increment>,
+    ) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+        Ok(())
+    }
+
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space=16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)] 
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account] 
+pub struct BaseAccount {
+    pub count: u64,
+}
--- tests/mysolanaapp.ts
+++ tests/mysolanaapp.ts
@@ -1,16 +1,45 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { assert } from "chai";
 import { Mysolanaapp } from "../target/types/mysolanaapp";
 
 describe("mysolanaapp", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Mysolanaapp;
+  let _baseAccount = anchor.web3.Keypair.generate();
+  const SystemProgram = anchor.web3.SystemProgram;
+  it("Create a counter", async () => {
+    
+    const baseAccount = anchor.web3.Keypair.generate();
+    
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
 
-  const program = anchor.workspace.Mysolanaapp as Program<Mysolanaapp>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 0: ', account.count.toString())
+    assert.ok(account.count.toString() == 0);
+    _baseAccount = baseAccount;
   });
+
+  it("increments the counter", async () => {
+    const baseAccount = _baseAccount;
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("Count 1", account.count.toString());
+    assert.ok(account.count.toString() == 1);
+  });
+  
 });
